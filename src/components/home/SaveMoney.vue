2432d3357b260df519c5a1b28ea810fa72ae9811
Eric Wang <msexploiting@gmail.com>
2021-03-21 17:45:40 -0500

    Old Save money section

---
+++ src/components/home/SaveMoney.vue
@@ -0,0 +1,40 @@
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SaveMoney",
+});
+</script>
+
+<template>
+  <div class="bg-white">
+    <div class="max-w-7xl mx-auto py-5 lg:p-20">
+      <h1 class="font-semibold text-4xl lg:w-2/3">
+        <span class="text-red-600 underline">Not convinced?</span> We save you
+        thousands of dollars from the beginning you purchase it to the end of
+        life.
+      </h1>
+
+      <div class="flex lg:py-10 lg:px-36 lg:flex-row lg:space-x-10">
+        <div class="lg:w-1/2 lg:p-10 bg-gray-50 rounded-md shadow-lg">
+          <h1 class="text-2xl font-bold text-center">Toyota Rav4</h1>
+          <div class="flex items-center w-full h-56">
+            <img src="@/assets/rav4.webp" alt="Toyota Rav4" />
+          </div>
+          <ul class="text-lg">
+            <li>Upfront cost</li>
+          </ul>
+        </div>
+        <div class="lg:w-1/2 lg:p-10 bg-gray-50 rounded-md shadow-lg">
+          <h1 class="text-2xl font-bold text-center">PWR Vite</h1>
+          <div class="flex items-center w-full h-56">
+            <img src="@/assets/bike.png" alt="Toyota Rav4" />
+          </div>
+          <ul>
+            <li>Upfront cost</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <v-hero />
   <one-bike-unlimited-possibilities />
-  <learn-how-much-savings />
+  <learn-how-much-savings class="mt-4" />
+  <save-money />
 </template>
 
 <script lang="ts">
@@ -9,13 +10,15 @@ import { defineComponent } from "vue";
 import VHero from "@/components/home/Hero.vue";
 import OneBikeUnlimitedPossibilities from "@/components/home/OneBikeUnlimitedPossibilities.vue";
 import LearnHowMuchSavings from "@/components/home/LearnHowMuchSavings.vue";
+import SaveMoney from "@/components/home/SaveMoney.vue"
 
 export default defineComponent({
   name: "Home",
   components: {
     VHero,
     OneBikeUnlimitedPossibilities,
-    LearnHowMuchSavings
+    LearnHowMuchSavings,
+    SaveMoney
   },
 });
 </script>
